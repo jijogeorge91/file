ea1aa0b3227b9a8a19e4a41e1a2c33df68c9ec58
root <root@ip-172-31-45-79.us-east-2.compute.internal>
2018-07-22 16:36:46 +0000

    i am commiting from branch

---
+++ dude.py
@@ -0,0 +1,5 @@
+<h>Welcome to ooty!
+Nice to meet you</h1>
+:SQ!
+
+get lose.full confusing
--- print.py
+++ print.py
@@ -3,3 +3,12 @@ Nice to meet you
 :by Jijo George
 
 This is not the first time i am telling :
+
+
+Arathy Roy
+
+I am editing this file to check more about the branches 
+hope i need to get little more to understand indetails:
+best reagrds
+Jijo George
+i don't have any idea that how it can be reflected in the master:
