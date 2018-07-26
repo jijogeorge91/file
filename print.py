830a3a2306177fda1f76ac87df514b086adf0814
root <root@ip-10-0-10-145.us-east-2.compute.internal>
2018-07-26 11:12:43 +0000

    editted print

--- print.py
+++ print.py
@@ -7,8 +7,3 @@ This is not the first time i am telling :
 
 Arathy Roy
 
-I am editing this file to check more about the branches 
-hope i need to get little more to understand indetails:
-best reagrds
-Jijo George
-i don't have any idea that how it can be reflected in the master:
